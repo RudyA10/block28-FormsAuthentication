825a9bd87f983d0ed7c18a4ccfe726eb68e46087
Rudy Acosta <rudyacosta323@gmail.com>
2025-09-15 19:34:08 -0500

    Final changes for submission

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -9,10 +9,44 @@ export function AuthProvider({ children }) {
   const [location, setLocation] = useState("GATE");
 
   // TODO: signup
+  async function signup(username) {
+    try {
+      const response = await fetch(`${API}/signup`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username }),
+      });
+
+      const result = await response.json();
+
+      if (response.ok) {
+        setToken(result.token);
+        setLocation("TABLET");
+      } else {
+        console.error("Signup failed:", result.message);
+      }
+    } catch (err) {
+      console.error("Error during signup:", err);
+    }
+  }
 
   // TODO: authenticate
+  const authenticate = async () => {
+    try {
+      if (!token) throw Error("No token found.");
+      const response = await fetch(API + "/authenticate", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (!response.ok) throw Error("Authentication failed.");
+      setLocation("TUNNEL");
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-  const value = { location };
+  const value = { signup, authenticate, location };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
